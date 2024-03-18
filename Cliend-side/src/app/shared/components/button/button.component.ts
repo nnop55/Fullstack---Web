7cf7d85be0a379a269b91b9384eb52f906fe1148
Tornike Melikishvili <tornikevasiko@gmail.com>
2024-03-18 21:39:54 +0400

    any

--- Cliend-side/src/app/shared/components/button/button.component.ts
+++ Cliend-side/src/app/shared/components/button/button.component.ts
@@ -9,6 +9,7 @@ export class ButtonComponent {
   @Input() label: string = 'Button';
   @Input() className: string = '';
   @Input() disabled: boolean = false;
+  @Input() btnType: 'submit' | 'button' = 'button';
 
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
 
