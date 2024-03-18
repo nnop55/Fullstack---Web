4d3cf40889a3e11d583c94e586c02ab8e8c53864
Tornike Melikishvili <tornikevasiko@gmail.com>
2024-03-18 20:34:16 +0400

    any

--- Cliend-side/src/app/shared/components/text-input/text-input.component.ts
+++ Cliend-side/src/app/shared/components/text-input/text-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-input',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class TextInputComponent {
 
+  @Input() placeholder: string = '';
+  @Input() inpType: 'submit' | 'text' = 'text';
+  @Input() className: string = '';
+
+  private _innerValue: any = '';
+
+  private onChangeCallback: (_: any) => void = () => { };
+  private onTouchedCallback: () => void = () => { };
+
+  constructor() { }
+
+  writeValue(value: any): void {
+    if (value !== this._innerValue) {
+      this._innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void { }
+
+  get innerValue(): any {
+    return this._innerValue;
+  }
+
+  set innerValue(value: any) {
+    if (value !== this._innerValue) {
+      this._innerValue = value;
+      this.onChangeCallback(this._innerValue);
+    }
+  }
+
+  onInputChange(event: any): void {
+    this.innerValue = event.target.value;
+  }
+
+  onBlur(): void {
+    this.onTouchedCallback();
+  }
 }
