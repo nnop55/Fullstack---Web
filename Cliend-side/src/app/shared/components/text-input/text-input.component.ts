adff377c32a4da070154a651d39cb2f0d4160bbf
Tornike Melikishvili <tornikevasiko@gmail.com>
2024-03-18 20:36:53 +0400

    s

--- Cliend-side/src/app/shared/components/text-input/text-input.component.ts
+++ Cliend-side/src/app/shared/components/text-input/text-input.component.ts
@@ -1,9 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.scss']
+  styleUrls: ['./text-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextInputComponent),
+      multi: true
+    }
+  ]
 })
 export class TextInputComponent {
 
