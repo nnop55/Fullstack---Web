8e7022c58901829dd65e03be834336f81ee905ff
nnop55 <tornikevasiko@gmail.com>
2024-05-19 20:38:10 +0400

    s

--- Back-End/src/utils/token.ts
+++ Back-End/src/utils/token.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 import { JWTSecretKey } from '../config/config';
 
-export function getToken(user: { id: number, email: string }, time: string = '1m') {
+export function getToken(user: { id: number, email: string }, time: string = '6h') {
     return jwt.sign(user, JWTSecretKey!, { expiresIn: time });
 }
 
