b4deb5aeab5037e9418b25155990273f4103540b
nnop55 <tornikevasiko@gmail.com>
2024-05-19 20:35:10 +0400

    s

--- client-side/src/app/core/guards/auth.guard.ts
+++ client-side/src/app/core/guards/auth.guard.ts
@@ -1,12 +1,10 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
-import { LocalStorageService } from '../services/local-storage.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const ls = inject(LocalStorageService)
 
   const not = () => {
     router.navigate(['/sign/in']);
